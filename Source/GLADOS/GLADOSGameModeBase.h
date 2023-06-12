8eab51edf7c698fd1460b13b1fb8ac35b0d26dfb
Kyner Grim <kghm1231@gmail.com>
2023-06-12 09:45:50 +0900

    Initial commit

---
+++ Source/GLADOS/GLADOSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GLADOSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADOS_API AGLADOSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
